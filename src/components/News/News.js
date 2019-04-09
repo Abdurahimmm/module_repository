6a51b2b69f96c14805161c7a94aca43d93908741
Abdurahim <daniyarbekovv@gmail.com>
2019-04-09 12:03:56 +0400

    News js begin axios.get

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import axios from '../../axios-news.js';
 
 class News extends Component {
+
+    state = {
+        title: '',
+        text: ''
+    }
+    componentDidMount() {
+        axios.get('/news.json')
+            .then(response => {
+                this.setState({
+                    title: '',
+                    text: ''
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     render() {
         return (
             <div>
-                Component News Here
+                <div>
+                    <h1>Title here</h1>
+                    <p>Text here</p>
+
+                </div>
             </div>
         );
     }
